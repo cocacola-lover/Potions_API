7b62feda910af78f6c514c02d25b27e4dd4dcf28
cocacola-lover <pegas620570@gmail.com>
2023-01-05 23:48:50 +0300

    Fixed mistakes and added id for insertions

--- database/js/makeQueries.js
+++ database/js/makeQueries.js
@@ -6,19 +6,19 @@ function insertIntoPotionQuality({ Tier, S0, S1, S2, S3, S4, S5 }) {
 }
 exports.insertIntoPotionQuality = insertIntoPotionQuality;
 function insertIntoPotions({ Potion, A, B, C, D, E, Type }) {
-    return `INSERT INTO Potions (potionName, potionType, A, B, C, D, E) VALUE ('${Potion}', ${Type}, ${A}, ${B}, ${C}, ${D}, ${E});`;
+    return `INSERT INTO Potions (potionName, potionType, A, B, C, D, E) VALUE ("${Potion}", ${Type}, ${A}, ${B}, ${C}, ${D}, ${E});`;
 }
 exports.insertIntoPotions = insertIntoPotions;
 function insertIntoIngredients({ Ingredient, Rarity, A, B, C, D, E, Total, BasePrice, Taste, Sensation, Aroma, Visual, Sound, Type, Location }) {
-    return `INSERT INTO Potions (name, rarity, ingredientType, A, B, C, D, E, total, taste, sensation, aroma, visual, sound, base_price, ingredientOrigin) VALUE ('${Ingredient}', ${Rarity}, ${Type}, ${A}, ${B}, ${C}, ${D}, ${E}, ${Total}, ${Taste}, ${Sensation}, ${Aroma}, ${Visual}, ${Sound}, ${BasePrice}, ${Location});`;
+    return `INSERT INTO Ingredients (name, rarity, ingredientType, A, B, C, D, E, total, taste, sensation, aroma, visual, sound, base_price, ingredientOrigin) VALUE ("${Ingredient}", ${Rarity}, ${Type}, ${A}, ${B}, ${C}, ${D}, ${E}, ${Total}, ${Taste}, ${Sensation}, ${Aroma}, ${Visual}, ${Sound}, ${BasePrice}, ${Location});`;
 }
 exports.insertIntoIngredients = insertIntoIngredients;
-function insertIntoIngredientType(ingredientType) {
-    return `INSERT INTO IngredientType (ingredientType) VALUE ('${ingredientType}');`;
+function insertIntoIngredientType(id, ingredientType) {
+    return `INSERT INTO IngredientType (id, ingredientType) VALUE (${id}, '${ingredientType}');`;
 }
 exports.insertIntoIngredientType = insertIntoIngredientType;
-function insertIntoIngredientOrigin(ingredientOrigin) {
-    return `INSERT INTO IngredientOrigin (ingredientOrigin) VALUE ('${ingredientOrigin}');`;
+function insertIntoIngredientOrigin(id, ingredientOrigin) {
+    return `INSERT INTO IngredientOrigin (id, ingredientOrigin) VALUE (${id}, '${ingredientOrigin}');`;
 }
 exports.insertIntoIngredientOrigin = insertIntoIngredientOrigin;
 function insertIntoPotionType(potionType) {
--- database/ts/makeQueries.ts
+++ database/ts/makeQueries.ts
@@ -3,19 +3,19 @@ function insertIntoPotionQuality({Tier, S0 , S1 , S2, S3, S4, S5} : Quality) {
 }
 
 function insertIntoPotions ( {Potion, A, B, C, D, E, Type} : Potion) {
-    return `INSERT INTO Potions (potionName, potionType, A, B, C, D, E) VALUE ('${Potion}', ${Type}, ${A}, ${B}, ${C}, ${D}, ${E});`
+    return `INSERT INTO Potions (potionName, potionType, A, B, C, D, E) VALUE ("${Potion}", ${Type}, ${A}, ${B}, ${C}, ${D}, ${E});`
 }
 
 function insertIntoIngredients ( {Ingredient, Rarity, A, B,C, D, E, Total, BasePrice,Taste, Sensation, Aroma, Visual, Sound, Type, Location} : Ingredient) {
-    return `INSERT INTO Potions (name, rarity, ingredientType, A, B, C, D, E, total, taste, sensation, aroma, visual, sound, base_price, ingredientOrigin) VALUE ('${Ingredient}', ${Rarity}, ${Type}, ${A}, ${B}, ${C}, ${D}, ${E}, ${Total}, ${Taste}, ${Sensation}, ${Aroma}, ${Visual}, ${Sound}, ${BasePrice}, ${Location});`
+    return `INSERT INTO Ingredients (name, rarity, ingredientType, A, B, C, D, E, total, taste, sensation, aroma, visual, sound, base_price, ingredientOrigin) VALUE ("${Ingredient}", ${Rarity}, ${Type}, ${A}, ${B}, ${C}, ${D}, ${E}, ${Total}, ${Taste}, ${Sensation}, ${Aroma}, ${Visual}, ${Sound}, ${BasePrice}, ${Location});`
 }
 
-function insertIntoIngredientType(ingredientType : string) {
-    return `INSERT INTO IngredientType (ingredientType) VALUE ('${ingredientType}');`;
+function insertIntoIngredientType(id: number, ingredientType : string) {
+    return `INSERT INTO IngredientType (id, ingredientType) VALUE (${id}, '${ingredientType}');`;
 }
 
-function insertIntoIngredientOrigin(ingredientOrigin : string) {
-    return `INSERT INTO IngredientOrigin (ingredientOrigin) VALUE ('${ingredientOrigin}');`;
+function insertIntoIngredientOrigin(id : number, ingredientOrigin : string) {
+    return `INSERT INTO IngredientOrigin (id, ingredientOrigin) VALUE (${id}, '${ingredientOrigin}');`;
 }
 
 function insertIntoPotionType(potionType : string) {
