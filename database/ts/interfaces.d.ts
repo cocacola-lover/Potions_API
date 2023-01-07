a9e9e1e31b09a65ec3342c2cc6303ba8bb26871b
cocacola-lover <pegas620570@gmail.com>
2023-01-07 23:24:02 +0300

    New input interface

--- database/ts/interfaces.d.ts
+++ database/ts/interfaces.d.ts
@@ -1,17 +1,21 @@
 interface Potion {
     Potion: string,
-    A, B, C, D, E : number,
+    A : number, B : number, C : number, D : number, E : number,
     Type : string | number
 }
 
 interface Quality {
     Tier : string,
-    S0, S1, S2, S3, S4, S5 : number
+    S0 : number, S1 : number, S2 : number, S3 : number, S4 : number, S5 : number
 }
 
 interface Ingredient {
     Ingredient : string,
-    Rarity, A, B,C, D, E, Total, BasePrice : number,
-    Taste, Sensation, Aroma, Visual, Sound : boolean,
-    Type, Location : string | number
+    Rarity : number, A : number, B : number, C : number, D : number, E : number, Total : number, BasePrice : number,
+    Taste : boolean, Sensation : boolean, Aroma : boolean, Visual : boolean, Sound : boolean,
+    Type : string | number, Location : string | number
 }
+
+interface mainInput {
+    host : string, user : string, password : string, database : string;
+  }
